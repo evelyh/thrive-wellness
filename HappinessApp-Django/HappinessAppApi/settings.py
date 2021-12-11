26e9d27e8cef1abf154b202ad420b159d0796afc
Eve Lei <evelyh.1217@gmail.com>
2021-12-10 19:10:29 -0500

    fixed media root

--- HappinessApp-Django/HappinessAppApi/settings.py
+++ HappinessApp-Django/HappinessAppApi/settings.py
@@ -14,6 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+REPOSITORY_ROOT = os.path.dirname(BASE_DIR)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
@@ -136,7 +137,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(REPOSITORY_ROOT, 'static/')
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(REPOSITORY_ROOT, 'media/')
+
 
 AUTH_USER_MODEL = 'users.User'
 
